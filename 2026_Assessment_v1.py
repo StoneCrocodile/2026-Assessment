3af404a869453d1cc858c94a6ad6a1b6d58beec7
StoneCrocodile <julianm24924@masseyhigh.school.nz>
2026-06-05 13:27:59 +1200

    Updated base

--- 2026_Assessment_v1.py
+++ 2026_Assessment_v1.py
@@ -1,4 +1,5 @@
-
+import pandas
+import tabulate
 
 
 def make_statement(statement, decoration):
@@ -33,7 +34,10 @@ def instructions():
     - The costs for each of the products you are comparing
       (variable expenses)
     - the unit of the item (kg,L, or per item)
-    - 
+
+The data will be put into a table and tell you which item is 
+the cheaper option 
+
 The data will also be written to a text file which has the 
 same name as your product and today's date.
 
@@ -85,7 +89,100 @@ def num_check(question, num_type="float", exit_code=None):
             print(error)
 
 
+def get_expenses(exp_type, how_many=1):
+    """Gets variable / fixed expenses and outputs
+    panda (as a string) and a subtotal of the expenses"""
+
+    # Lists for panda
+    all_items = []
+    all_amounts = []
+    all_dollar_per_item = []
+
+    # Expenses dictionary
+    expenses_dict = {
+        "Item": all_items,
+        "Amount": all_amounts,
+        "$ / Item": all_dollar_per_item
+    }
+
+    # defaults for fixed expenses
+    amount = how_many  # how_many defaults to 1
+    how_much_question = "How much? $"
+
+    # loop to get expenses
+    while True:
+
+        # Get item name and check it's not blank
+        item_name = not_blank("Item Name: ")
+
+        # check users enter at least one variable expense
+        if exp_type == "variable" and item_name == "xxx" and len(all_items) == 0:
+            print("Oops - you have not entered anything.  "
+                  "You need at least one item.")
+            continue
+
+        # end loop when users enter exit code
+        elif item_name == "xxx":
+            break
+
+        # Get variable expenses item amount <enter> defaults to number of
+        # products being made.
+        if exp_type == "variable":
+
+            amount = num_check(f"How many <enter for {how_many}>: ",
+                               "integer", "")
+
+            # Allow users to push <enter> to default to number of items being made
+            if amount == "":
+                amount = how_many
+
+            how_much_question = "Price for one? $"
+
+        # Get price for item (question customised depending on expense type).
+        price_for_one = num_check(how_much_question, "float")
+        print()
+
+        all_items.append(item_name)
+        all_amounts.append(amount)
+        all_dollar_per_item.append(price_for_one)
+
+    # make panda
+    expense_frame = pandas.DataFrame(expenses_dict)
+
+    # Calculate Cost Column
+    expense_frame['Cost'] = expense_frame['Amount'] * expense_frame['$ / Item']
+
+    # Apply currency formatting to currency columns.
+    add_dollars = ['Amount', '$ / Item', 'Cost']
+    for var_item in add_dollars:
+        expense_frame[var_item] = expense_frame[var_item].apply(currency)
+
+
 def currency(x):
     """Formats numbers as currency ($#.##)"""
     return "${:.2f}".format(x)
 
+# Main Routine
+
+# assume we have no fixed expenses for now
+fixed_subtotal = 0
+fixed_panda_string = ""
+
+print(make_statement("Price Comparison Calculator", "-"))
+
+print()
+want_instructions = yes_no_check("Do you want to see the instructions? ")
+print()
+
+if want_instructions == "yes":
+    instructions()
+
+print()
+
+# Get product details...
+product_name = not_blank("Product Name: ")
+quantity_made = num_check("Quantity being ordered: ", "integer")
+price = num_check("Price: $", "integer")
+
+price_per_item = quantity_made * price
+print(f"The total price of {quantity_made} {product_name}'s is $",price_per_item)
